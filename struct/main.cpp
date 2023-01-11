db02355ad2d2cff28edf912aa8aca721be8bf2fa
Texas-NO1 <25841019@qq.com>
2023-01-11 15:30:32 +0800

    finish rb tree

--- struct/main.cpp
+++ struct/main.cpp
@@ -2,39 +2,25 @@
 #include <iostream>
 #include <queue>
 #include <cassert>
+#include <map>
+#include <unordered_set>
 
 //     1
 // 0       2
 //             5 
 //           4      
-    
-
-int check_bh(RB_Node *root) {
-    if (root == RB_Tree::nil) return 1;
-    int lbh = check_bh(root->left);
-    int rbh = check_bh(root->right);
-    assert(lbh == rbh);
-    return std::max(lbh, rbh) + (root->color == RB_Node::BLACK ? 1 : 0);
-}
-
-int main () {
-    RB_Tree t;
-    for (int i = 0; i < 1000; ++i) {
-        t.insert(rand() % 1000);
-    }
+void output(RB_Node *root) {
     std::queue<RB_Node*> que;
-    que.push(t.root);
+    que.push(root);
     while (!que.empty()) {
         std::queue<RB_Node*> temp;
         while (!que.empty()) {
             auto cur = que.front();
             que.pop();
             if (cur == RB_Tree::nil) {
-                std::cout << "(nil) ";
+                std::cout << "(" << (cur->color == RB_Node::BLACK ? "B_" : "R_") << "nil) ";
                 continue;
             }
-            if (cur->color == RB_Node::RED)
-            assert(cur->color != cur->p->color);
             std::cout << (cur->color == RB_Node::BLACK ? "B_" : "R_") << cur->key << ",";
             temp.push(cur->left);
             temp.push(cur->right);
@@ -42,6 +28,80 @@ int main () {
         que.swap(temp);
         std::cout << std::endl;
     }
-    check_bh(t.root);
+}
+
+int check_bh(RB_Node *root) {
+    if (root == RB_Tree::nil) return 1;
+    int lbh = check_bh(root->left);
+    int rbh = check_bh(root->right);
+    if (lbh != rbh) {
+        std::cout << lbh << "!=" << rbh << std::endl;
+        output(root);
+    }
+    assert(lbh == rbh);
+    return lbh + (root->color == RB_Node::BLACK ? 1 : 0);
+}
+
+bool check_color(RB_Node *root) {
+    if (root == RB_Tree::nil) return true;
+    bool lcolor = check_color(root->left);
+    bool rcolor = check_color(root->right);
+    bool cur_color = false;
+    switch (root->color) {
+        case RB_Node::BLACK : cur_color = true; break;
+        case RB_Node::RED : cur_color = true; break;
+        default: cur_color == false; break;
+    }
+    assert(lcolor && rcolor && cur_color);
+    return lcolor && rcolor && cur_color;
+}
+
+bool check_has_near_red_node(RB_Node *root) {
+    if (root == RB_Tree::nil) return false;
+    bool lcolor = check_color(root->left);
+    bool rcolor = check_color(root->right);
+    bool is_red_node = (root->color == RB_Node::RED);
+    bool has_red_child = false;
+    if (root->left != RB_Tree::nil && root->left->color == RB_Node::RED) has_red_child = true;
+    if (root->right != RB_Tree::nil && root->right->color == RB_Node::RED) has_red_child = true;
+    assert(!(is_red_node && has_red_child));
+    return is_red_node && has_red_child && lcolor && rcolor;
+}
+
+bool check_is_rb_tree(RB_Tree &t) {
+    RB_Node *root = t.root;
+    check_color(root); // 性质1
+    assert(root->color == RB_Node::BLACK); // 性质2
+    assert(RB_Tree::nil->color == RB_Node::BLACK); // 性质3
+    check_has_near_red_node(root); // 性质4
+    check_bh(root); // 性质5
+}
+
+int main () {
+    RB_Tree::init_nil();
+    int max = 100000;
+    std::vector<int> all_items;
+    RB_Tree t;
+    for (int i = 0; i < 1000; ++i) {
+        int x = rand() % max + 1;
+        t.insert(x);
+        all_items.push_back(x);
+    }
+    // output(t);
+    check_is_rb_tree(t);
+
+    // delete check
+    // output(t.root);
+
+    for (auto i : all_items) {
+        // std::cout << "----org---" << std::endl;
+        // output(t.search(i));
+        t.remove(i);
+        // std::cout << "----remove---" << std::endl;
+        // output(new_y);
+        check_is_rb_tree(t);
+    }
+    output(t.root);
+    check_is_rb_tree(t);
     return 0;
 }
--- struct/rb-tree.hpp
+++ struct/rb-tree.hpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cassert>
+
 struct RB_Node {
     enum RB_COLOR{RED, BLACK};
     RB_COLOR color;
@@ -103,6 +105,73 @@ private:
         root->color = RB_Node::BLACK;
     }
 
+    void transplant(RB_Node *u, RB_Node *v) {
+        if (u->p == nil) {
+            root = v;
+        } else if (u->p->left == u) {
+            u->p->left = v;
+        } else {
+            u->p->right = v;
+        }
+        v->p = u->p;
+    }
+
+    void remove_fixup(RB_Node *x) {
+        RB_Node *w = nil;
+        while ((x != root) && (x->color == RB_Node::BLACK)) {
+            if (x == x->p->left) {
+                w = x->p->right;
+                if (w->color == RB_Node::RED) {
+                    w->color = RB_Node::BLACK;
+                    x->p->color = RB_Node::RED;
+                    left_rotate(x->p);
+                    w = x->p->right;
+                }
+                if (w->left->color == RB_Node::BLACK && w->right->color == RB_Node::BLACK) { // 如果x->p为红怎么办
+                    w->color = RB_Node::RED;
+                    x = x->p;
+                } else {
+                    if (w->right->color == RB_Node::BLACK) {
+                        w->left->color = RB_Node::BLACK;
+                        w->color = RB_Node::RED;
+                        right_rotate(w);
+                        w = x->p->right;
+                    }
+                    w->color = x->p->color;
+                    x->p->color = RB_Node::BLACK;
+                    w->right->color = RB_Node::BLACK;
+                    left_rotate(x->p);
+                    x = root;
+                }
+            } else {
+                w = x->p->left;
+                if (w->color == RB_Node::RED) {
+                    w->color = RB_Node::BLACK;
+                    x->p->color = RB_Node::RED;
+                    right_rotate(x->p);
+                    w = x->p->left;
+                }
+                if (w->left->color == RB_Node::BLACK && w->right->color == RB_Node::BLACK) { // 如果x->p为红怎么办
+                    w->color = RB_Node::RED;
+                    x = x->p;
+                } else {
+                    if (w->left->color == RB_Node::BLACK) {
+                        w->right->color = RB_Node::BLACK;
+                        w->color = RB_Node::RED;
+                        left_rotate(w);
+                        w = x->p->left;
+                    }
+                    w->color = x->p->color;
+                    x->p->color = RB_Node::BLACK;
+                    w->left->color = RB_Node::BLACK;
+                    right_rotate(x->p);
+                    x = root;
+                }
+            }
+        }
+        x->color = RB_Node::BLACK;
+    }
+
 public:
     static RB_Node *nil;
     RB_Node *root;
@@ -156,6 +225,56 @@ public:
         }
         // std::cout << "cur key " << z->key << ", p " << z->p->key << "'s " << (z == z->p->left ? "L" : "R") << std::endl;
         insert_fixup(z);
+    }
+
+    void remove(int key) {
+        // std::cout << "remove " << key << std::endl;
+        RB_Node *z = root;
+        while (z->key != key && z != nil) {
+            if (key < z->key) {
+                z = z->left;
+            } else if (key > z->key) {
+                z = z->right;
+            }
+        }
+        if (z == nil) return;
+
+        RB_Node *x = nil;
+        RB_Node *y = z;
+        RB_Node::RB_COLOR y_org = y->color;
+        if (z->left == nil) {
+            x = z->right;
+            transplant(z, z->right);
+        } else if (z->right == nil) {
+            x = z->left;
+            transplant(z, z->left);
+        } else {
+            y = min(z->right);
+            y_org = y->color;
+            x = y->right;
+            if (y->p == z) {
+                x->p = y;
+            } else {
+                transplant(y, y->right);
+                y->right = z->right;
+                y->right->p = y;
+            }
+            transplant(z, y);
+            y->left = z->left;
+            y->left->p = y;
+            y->color = z->color;
+        }
+        // std::cout << "y " << y->key << ", x " << x->key << std::endl;
+        // std::cout << "remove " << z->key << ", to fixup " << x->key << " isnil=" << (x == nil) << std::endl;
+        // std::cout << "x->p " << x->p->key << ", x->p->p " << x->p->p->key << std::endl; 
+        if (y_org == RB_Node::BLACK) {
+            remove_fixup(x);
+        }
+        // std::cout << "remove " << z->key << " over" << std::endl;
+    }
+    
+    static void init_nil() {
+        nil->left = nil->right = nil->p = nil;
     }
 };
 
