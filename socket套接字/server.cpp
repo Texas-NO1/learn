1cf1f2a69c520503c301810029a105063b107ebb
Texas-NO1 <25841019@qq.com>
2023-12-05 20:19:24 +0800

    epoll server有bug，TODO修复

--- socket套接字/server.cpp
+++ socket套接字/server.cpp
@@ -11,6 +11,7 @@
 #include <unordered_set>
 #include <vector>
 #include <thread>
+#include <unordered_map>
 #include <sys/epoll.h> // TODO多路复用（当前策略为一个客户端连接一个线程监听，连接过多时不可用）
 
 bool running;
@@ -31,34 +32,35 @@ private:
     std::string server_name; // 连接的服务端名字
     std::string client_name; // 连接的客户端名字
 public:
+    Connection() : fd(-1), is_connected(false) {}
     Connection(int fd, std::string name, std::string server_name, std::string client_name) : 
     fd(fd), name(name), server_name(server_name), client_name(client_name){
         is_connected = true;
         printf("Create Connection fd %d, name %s, connect %s - %s\n", fd, name.data(), server_name.data(), client_name.data());
     }
 
+    // 返回true，表示已读完
+    // 默认情况下socket fd的read为blocking模式，只有在连接失效时才会返回0，因为一直blocking读取所以不会读取到EOF
+    // 当设置read为非blocking模式时，如果读到空的则会立即返回0
     bool read(std::string &receive, int read_batch = 100) { // 一次读多少字符
         std::string rec(read_batch, 0);
         // printf("s\n");
         int cnt = ::read(fd, (void *)rec.data(), rec.size()); // 4. 从连接的套接字读取数据到buff
         // printf("e\n");
-        assert(cnt >= 0);
         receive = rec.substr(0, cnt);
         // printf("rec %d chars\n", cnt);
+        // blocking模式下read会返回0，非blocking模式下只会在连接断开时才会返回0
         return cnt == 0;
     }
 
-    void continueReading() {
-        printf("(Client %s Connection %s) start reading\n",
-            client_name.data(),
-            name.data());
-        while (is_connected) {
-            // printf("reading\n");
-            std::string msg;
-            if (read(msg)) close(); // connaction close since socket closed
-            if (!msg.empty()) printf("\n%s\n", msg.data());
-            printf("type r to reply");
+    // 一次性读取所有内容
+    std::string read() {
+        std::string res;
+        std::string msg;
+        while (!read(msg)) {
+            res += msg;
         }
+        return res;
     }
 
     void write(std::string input) {
@@ -92,12 +94,12 @@ private:
     int fd; // socket文件描述符
     int pid; // 进程pid
     std::string name; // 给server一个名字
-    std::vector<Connection> conns;
-    std::vector<std::thread> listen_threads;
+    std::unordered_map<int, Connection> fd2conns;
     std::thread write_thread; // 另起一个线程写数据
+    bool is_running;
 
     std::string genNewConnName() {
-        return "connection-" + std::to_string(conns.size());
+        return "connection-" + std::to_string(fd2conns.size());
     }
 public:
     Server(std::string name, std::string host, int port, int max_conn = 1) {
@@ -123,20 +125,66 @@ public:
         assert(status >= 0);
         status = listen(fd, max_conn); // 4. 监听套接字
         assert(status >= 0);
+        is_running = true;
     }
 
-    // 等待连接
-    void waitting() {
-        int new_fd = accept(fd, nullptr, nullptr); // 这里addr可能获取不到，直接从fd中getsockname
-        struct sockaddr_in addr;
-        socklen_t size = 0;
-        getsockname(new_fd, (sockaddr *)&addr, &size);
-        assert(new_fd >= 0);
-        std::string client_addr = inet_ntoa(addr.sin_addr);
-        client_addr += ":" + std::to_string(addr.sin_port);
-        // printf("get a new connection from %s address size %d\n", client_addr.data(), size);
-        conns.push_back(std::move(Connection(new_fd, genNewConnName(), name, client_addr)));
-        listen_threads.push_back(std::thread(&Connection::continueReading, conns.back()));
+    void setnonblocking(int fd) {
+        int flags = fcntl(fd, F_GETFL);
+        assert(flags >= 0);
+        flags |= O_NONBLOCK;
+        int res = fcntl(fd, F_SETFL, flags);
+        assert(res >= 0);
+    }
+
+    // 使用epoll，仅用一个线程维持所有连接
+    void start() {
+        int epollfd = epoll_create1(0);
+        assert(epollfd >= 0);
+        printf("create a epoll fd %d to control all connection\n", epollfd);
+        struct epoll_event ev;
+        ev.events = EPOLLIN; // EPOLLIN监听进事件(文件可读时)
+        ev.data.fd = fd;
+        int status = epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &ev); // EPOLL_CTL_ADD添加一个fd(socket)到epollfd管理
+        assert(status >= 0);
+        int events_batch = 100; // 设一次最大获取100个事件
+        std::vector<struct epoll_event> events(events_batch);
+        while (is_running) {
+            int nfds = epoll_wait(epollfd, events.data(), events_batch, -1); // -1表示block住直到有事件发生
+            assert(nfds >= 0);
+            for (int i = 0; i < nfds; ++i) {
+                // 事件为server的fd，表示有新连接
+                if (events[i].data.fd == fd) {
+                    int new_fd = accept(fd, nullptr, nullptr);
+                    assert(new_fd >= 0);
+                    struct sockaddr_in addr;
+                    socklen_t size = 0;
+                    getsockname(new_fd, (sockaddr *)&addr, &size);
+                    std::string client_addr = inet_ntoa(addr.sin_addr);
+                    client_addr += ":" + std::to_string(addr.sin_port);
+                    fd2conns[new_fd] = std::move(Connection(new_fd, genNewConnName(), name, client_addr));
+                    // 连接不可阻塞，否则会一直卡在读取某个连接上
+                    setnonblocking(new_fd);
+                    struct epoll_event new_ev;
+                    // 可读事件 + 边缘触发(EdgeTrigger) + 错误事件
+                    // 边缘触发表示，一次事件仅发送一次通知，下次新事件再来时才会重新发送通知
+                    // 所以边缘触发时一定要将所有数据循环读完，因为一次收到的数据可能会大于的buff size，需要不断循环获取完所有数据再退出
+                    new_ev.events = EPOLLIN | EPOLLET | EPOLLERR;
+                    new_ev.data.fd = new_fd;
+                    status = epoll_ctl(epollfd, EPOLL_CTL_ADD, new_fd, &new_ev);
+                    assert(status >= 0);
+                } else {
+                    // 事件为connection的fd，表示连接有新数据或者连接有问题
+                    int event_fd = events[i].data.fd;
+                    if (events[i].events == EPOLLIN) {
+                        // 连接有数据进来，读取并输出
+                        std::string msg = fd2conns[event_fd].read();
+                        if (!msg.empty()) printf("\n%s\n", msg.data());
+                    } else {
+                        printf("conn fd %d has error\n", event_fd);
+                    }
+                }
+            }
+        }
     }
 
     void continueWriting() {
@@ -157,10 +205,10 @@ public:
                 if (op == "reply") {
                     printf("\nConnection List: \n");
                     std::unordered_set<int> support_conns;
-                    for (int i = 0; i < this->conns.size(); ++i) {
-                        printf("%d : %s ", i, this->conns[i].getName().data());
-                        if (this->conns[i].isClosed()) printf("(closed)"); // TODO delete closed conn
-                        else support_conns.insert(i);
+                    for (auto &fd2conn : fd2conns) {
+                        printf("%d : %s ", fd2conn.first, fd2conn.second.getName().data());
+                        if (fd2conn.second.isClosed()) printf("(closed)"); // TODO delete closed conn
+                        else support_conns.insert(fd2conn.first);
                         printf("\n");
                     }
                     printf("You can choose one client to reply: ");
@@ -170,7 +218,7 @@ public:
                         printf("client choosed has been closed or not exsists\n");
                         continue;
                     }
-                    choose_conn = &(this->conns[reply_idx]);
+                    choose_conn = &(fd2conns[reply_idx]);
                     printf("reply: ");
                     std::string reply_str;
                     std::cin >> reply_str;
@@ -181,13 +229,12 @@ public:
     }
 
     ~Server() {
+        is_running = false;
         printf("Destroy Server, close fd %d\n", fd);
         int status = close(fd);
         assert(status >= 0);
-        for (int i = 0; i < conns.size(); ++i) {
-            conns[i].close();
-            if (listen_threads[i].joinable())
-                listen_threads[i].join();
+        for (auto &fd2conn : fd2conns) {
+            fd2conn.second.close();
         }
     }
 };
@@ -198,11 +245,8 @@ int main () {
     signal(SIGSEGV, signalHandler);
     signal(SIGABRT, signalHandler);
     signal(SIGKILL, signalHandler); // 不能捕捉
-    running = true;
     Server server("qxai-server-"+ std::to_string(getpid()), "0.0.0.0", 12345);
     server.continueWriting();
-    while (running) {
-        server.waitting();
-    }
+    server.start();
     return 0;
 }
